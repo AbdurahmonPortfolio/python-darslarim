887cfe8368a2d300e45837d56f6092471f955080
AbdurahmonPortfolio <parolikodi1234567@gmail.com>
2023-05-20 12:46:33 +0500

    14-dars vazifasi qo'shildi

---
+++ 14-dars-vazifalar.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 12:09:09 2023
+
+14-dars Lug'atlar bilan ishlash
+
+@author: User
+"""
+
+#ukam={'ism':'muhammadali','manzil':"marg'ilon",'yoshi':3}
+#print(f"Ukamning ismi {ukam['ism'].title()},tug'ilgan shahri {ukam['manzil'].title()},\
+# yoshi-{ukam['yoshi']} da")
+
+
+#taomlar={'jo':'osh','richr':'somsa','ser':'baliq',
+#         'jek':'shorva','po':'manti'}
+#tao=taomlar['ser']
+#print(f"serning sevimli taomi {tao}")
+
+
+#python={'if':'agar','int':'butun son','string':"so'z",
+ #       'else':'aks holda','or':"yoki"}
+#kalit=input("Kalit so'zni kiriting :").lower()
+#print(python.get(kalit,"Bunday so'z mavjud emas"))
+
+
+#kalit=input("Kalit so'zni kiriting :").lower()
+#tarjima=python.get(kalit)
+#if tarjima==None:
+#   print("Bunday so'z mavjud emas")
+#else:
+#    print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi")
+
+
+
