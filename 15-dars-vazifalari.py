800d906c6dbb0ad196141a9b4c1008b9f16820e5
AbdurahmonPortfolio <parolikodi1234567@gmail.com>
2023-05-22 17:12:18 +0500

    15-dars vazifalari qoshildi

---
+++ 15-dars-vazifalari.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 15:02:10 2023
+
+15-dars Lug'atlarni tekshirish
+
+@author: User
+"""
+#tiplar={'String':"So'zlar bilan ishlovchi tip",
+#      'int':'Butun son','float':"O'nlik son",
+#       'Boolen':'Mantiqiy qiymat','if':"Shartlarni tekshirovchi operator"}
+#for kalit,qiymat in tiplar.items():
+#    print(f"{kalit}-{qiymat}")
+
+
+#davlatlar={'aqsh':'vashinton','rossiya':'moskva',
+#           'saudiya arabistoni':'ar-riyod',"o'zbekiston":'toshkent',
+#           'syria':'damashq','isroil':'tel-avi','falastin':'quddus'}
+#print("Dunyo davlatlari")
+#for davlat in sorted(davlatlar):
+#    print(davlat.title())
+#    
+#print("\nDavlatlar poytaxtlari")
+#for poytaxt in sorted(davlatlar.values()):
+#    print(poytaxt.title())
+#
+#country = input('\nQaysi davlatning poytaxtini bilishni istaysiz?:').lower()
+#capital = davlatlar.get(country)
+#if capital==None:
+#    print('/nKechirasiz, bizda bu haqida ma\'lumot yo\'q')    
+#else:
+#    print(f"{country.upper()}ning poytaxti {capital.title()} shahri")
+
+
+#menu={'osh':20000,'non':3000,'choy':2000,
+#      'somsa':10000,'pirajoy':20000,'norin':15000,
+#      'lavash':20000,'shaurma':22000,'achuchu':5000}
+
+#print('3 ta taom buyurtma bering.')
+#buyurtmalar = []
+#for n in range(3):
+#    buyurtmalar.append(input(f"{n+1}-taom:").lower())
+
+#for buyurtma in buyurtmalar:
+#    if buyurtma in menu:
+#       print(f"{buyurtma.title()} {menu[buyurtma]} so'm")
+#    else:
+#        print(f"Kechirasiz, bizda {buyurtma} yo'q.")
